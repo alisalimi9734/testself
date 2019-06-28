15a86eae1e519e9bd19ad1034a3c54ac17488c21
alisalimi9734 <52218649+alisalimi9734@users.noreply.github.com>
2019-06-28 15:47:28 +0430

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,47 @@
+do local _ = {
+  about_text = "",
+  enabled_plugins = {
+    "FunTools",
+    "aparat",
+    "dl-file",
+    "fars",
+    "get",
+    "groupmanager",
+    "help",
+    "id",
+    "ip",
+    "on-off",
+    "onservice",
+    "plugins",
+    "rps",
+    "savefile",
+    "saveplug",
+    "set",
+    "Spammer",
+    "GPS",
+    "Terminal",
+    "weather",
+    "Instagram",
+    "Poker",
+    "fuck",
+    "porn",
+    "sticker-maker",
+    "vote",
+    "myid",
+    "myinfo",
+    "solid"
+  },
+  help_text = "",
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    732020140,
+    0,
+    0
+  }
+}
+return _
+end
